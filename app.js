5ea83dc28b81a4d58aadfd8d712b7efffe1b935b
Matthew932932 <184472697+Matthew932932@users.noreply.github.com>
2025-01-25 12:05:37 +1100

    completed controllers exercise

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ app.use("/", indexRouter);
 // app.get("/", (req, res) => res.send("Hello, world!"));
 
 
+// Every thrown error in the application or the previous middleware function calling `next` with an error as an argument will eventually go to this middleware function
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`My first Express app - listening on port ${PORT}!`);
